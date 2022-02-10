ab4b778ac2088e45622b05f09e194977b10876a6
Paipoi8387 <ats07288387@outlook.jp>
2022-02-10 17:07:33 +0900

    resource_storageに関するREADMEを変更しました。

--- resource_storage/resource_storage.sh
+++ resource_storage/resource_storage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #使用する低レベルランタイムを宣言
-declare -a low_level_runtime=("runc" "crun" "runsc")
+declare -a low_level_runtime=("runc" "crun" "runsc" "kata")
 #各低レベルランタイムについてコンテナを立ち上げる個数
 container_num=20
 #コンテナイメージの指定
